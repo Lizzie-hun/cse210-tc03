34e7df5bcd67de903a1d54a586ac8ead75ff1eda
Lizzie-hun <91574342+Lizzie-hun@users.noreply.github.com>
2021-10-02 13:34:20 -0600

    Update __init__.py

--- Week3_team/__init__.py
+++ Week3_team/__init__.py
@@ -1,7 +1,8 @@
 def first():
   print("Welcome to our collaborative program")
 
-  def greeting():
-    print('Hello from Bekah Bentley')
+def greeting():
+  print('Hello from Bekah Bentley')
 
- 
+def lizzie():
+  print("Hello from Lizzie!")
